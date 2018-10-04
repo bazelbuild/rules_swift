d519fd3fb5182a45005d29d32954b2d651b948cc
Dave Lee <davelee.com@gmail.com>
2018-10-04 09:59:18 -0700

    rework env variable handling

--- .travis/build.sh
+++ .travis/build.sh
@@ -51,8 +51,9 @@ if [[ -n "${BAZEL:-}" ]]; then
     ALL_TEST_ARGS+=("--test_tag_filters=${TAGS}")
   fi
 
-  bazel "${BAZELRC_ARGS[@]}" build "${ALL_BUILD_ARGS[@]}" -- ${TARGETS}
-  bazel "${BAZELRC_ARGS[@]}" test "${ALL_BUILD_ARGS[@]}" "${ALL_TEST_ARGS[@]}" -- ${TARGETS}
+  read -a targets <<< TARGETS
+  bazel "${BAZELRC_ARGS[@]}" build "${ALL_BUILD_ARGS[@]}" -- "${targets[@]}"
+  bazel "${BAZELRC_ARGS[@]}" test "${ALL_BUILD_ARGS[@]}" "${ALL_TEST_ARGS[@]}" -- "${targets[@]}"
   set +x
 fi
 
