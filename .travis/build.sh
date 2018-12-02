ff5842d42bd38ee5426f8256cadab821fcabf0d2
Dave Lee <davelee.com@gmail.com>
2018-12-01 22:58:36 -0800

    Run with --sandbox_debug

--- .travis/build.sh
+++ .travis/build.sh
@@ -40,6 +40,7 @@ if [[ -n "${BAZEL:-}" ]]; then
   ALL_BUILD_ARGS=(
       --show_progress_rate_limit=30.0
       --verbose_failures
+      --sandbox_debug
       --action_env=PATH
   )
   if [[ -n "${BUILD_ARGS:-}" ]]; then
