a1f3574e7a48335c15a28c5cbd5728498c45a6de
Keith Smiley <keithbsmiley@gmail.com>
2019-01-17 17:48:40 -0800

    Update travis to Xcode 10

--- .travis/build.sh
+++ .travis/build.sh
@@ -41,6 +41,7 @@ if [[ -n "${BAZEL:-}" ]]; then
       --show_progress_rate_limit=30.0
       --verbose_failures
       --action_env=PATH
+      --cpu=darwin_x86_64
   )
   if [[ -n "${BUILD_ARGS:-}" ]]; then
     ALL_BUILD_ARGS+=(${BUILD_ARGS})
