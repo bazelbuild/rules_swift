9678f1727e83b6c6752be7779a82a8c0e661899d
Keith Smiley <keithbsmiley@gmail.com>
2019-02-26 15:11:26 -0500

    Remove host_cpu flag
    `darwin` is now a valid macOS platform with bazel 0.23.0 which was the
    only reason we needed this override
    
    Closes #148.
    
    RELNOTES: None.
    PiperOrigin-RevId: 235767697

--- .travis/build.sh
+++ .travis/build.sh
@@ -43,11 +43,6 @@ if [[ -n "${BAZEL:-}" ]]; then
       --action_env=PATH
   )
 
-  # Can be removed when https://github.com/bazelbuild/bazel/pull/7151 is released
-  if [[ $OSTYPE == darwin* ]]; then
-    ALL_BUILD_ARGS+=("--host_cpu=darwin_x86_64")
-  fi
-
   if [[ -n "${BUILD_ARGS:-}" ]]; then
     ALL_BUILD_ARGS+=(${BUILD_ARGS})
   fi
