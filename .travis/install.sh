929b1ea6950a9842271ec4da96b2bff950ca50bf
thomasvl <thomasvl@google.com>
2019-03-04 11:13:39 -0500

    Travis CI Cleanup.
    Bazel's BuildKite seems to be fine for the bazel builds, so remove the support
    for doing bazel builds on Travis.
    
    Note: Keeping the matrix as a manual build and some of the conditions in the
    scripts just to reduce the chances of something not being right, as eventually
    the BuildKite setup will match the buildifier support and these files will
    just get dropped.
    
    RELNOTES: None
    PiperOrigin-RevId: 236655099

--- .travis/install.sh
+++ .travis/install.sh
@@ -35,47 +35,6 @@ function github_latest_release_tag() {
 }
 
 # ------------------------------------------------------------------------------
-# Helper to get a download url out of bazel build metadata file.
-function url_from_bazel_manifest() {
-  local MANIFEST_URL=$1
-    if [[ "${OS}" == "darwin" ]]; then
-      local JSON_OS="macos"
-    else
-      local JSON_OS="ubuntu1404"
-    fi
-  wget -O - "${MANIFEST_URL}" \
-    | python -c "import json; import sys; print json.load(sys.stdin)['platforms']['${JSON_OS}']['url']"
-}
-
-# ------------------------------------------------------------------------------
-# Helper to install bazel.
-function install_bazel() {
-  local VERSION="${1}"
-
-  if [[ "${VERSION}" == "RELEASE" ]]; then
-    VERSION="$(github_latest_release_tag bazelbuild/bazel)"
-  fi
-
-  # macOS and trusty images have jdk8, so install bazel without jdk.
-  if [[ "${VERSION}" == "HEAD" ]]; then
-    # bazelbuild/continuous-integration/issues/234 - they don't seem to have an installed
-    # just raw binaries?
-    mkdir -p "$HOME/bin"
-    wget -O "$HOME/bin/bazel" \
-      "$(url_from_bazel_manifest https://storage.googleapis.com/bazel-builds/metadata/latest.json)"
-    chmod +x "$HOME/bin/bazel"
-  else
-    wget -O install.sh \
-      "https://github.com/bazelbuild/bazel/releases/download/${VERSION}/bazel-${VERSION}-installer-${OS}-x86_64.sh"
-    chmod +x install.sh
-    ./install.sh --user
-    rm -f install.sh
-  fi
-
-  bazel version
-}
-
-# ------------------------------------------------------------------------------
 # Helper to install buildifier.
 function install_buildifier() {
   local VERSION="${1}"
@@ -102,32 +61,5 @@ function install_buildifier() {
 }
 
 # ------------------------------------------------------------------------------
-# Helper to install Swift.
-function install_swift() {
-  local VERSION="${1}"
-
-  mkdir .swift
-  curl -sL "https://swift.org/builds/swift-$VERSION-release/ubuntu1404/swift-$VERSION-RELEASE/swift-$VERSION-RELEASE-ubuntu14.04.tar.gz" | \
-      tar xz -C .swift &> /dev/null
-
-  # TODO(bazelbuild/bazel#6834): This is a workaround for an apparent issue with
-  # the C++ toolchain configuration. The repository rule runs
-  # `clang -fuse-ld=gold` in a repository rule context to determine if gold is
-  # supported as the linker, which (I believe) can find `ld.gold` on the system
-  # path. It also passes the path to Clang's bin directory as a program prefix
-  # directory (`-B`), but if `ld.gold` doesn't live in this directory, then when
-  # `clang` is invoked in a build action context, it won't have access to
-  # `PATH`, so the subsequent link action fails.
-  #
-  # There are two ways to fix this: pass `--action_env=PATH` to Bazel or symlink
-  # `ld.gold` where Clang can find it within Bazel's sandbox. We choose the
-  # latter because it doesn't leak the system path into every action.
-  which ld.gold && \
-    sudo ln -s "$(which ld.gold)" /usr/local/clang-5.0.0/bin/ld.gold
-}
-
-# ------------------------------------------------------------------------------
 # Install what is requested.
-[[ -z "${BAZEL:-}" ]] || install_bazel "${BAZEL}"
 [[ -z "${BUILDIFIER:-}" ]] || install_buildifier "${BUILDIFIER}"
-[[ -z "${SWIFT_VERSION:-}" ]] || install_swift "${SWIFT_VERSION}"
