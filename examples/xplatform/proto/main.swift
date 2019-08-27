1d12ac929b6b8186fb0439445f1a84ee1c6a767d
Thomas Van Lenten <thomasvl@google.com>
2019-08-27 11:29:31 -0700

    Expand the example to include WKT usage.
    Not only shows using the WKT a little, but also servers to exercise
    more of swift_proto_library since the WKTs get pulled in thru the runtime
    and thus are special handled in the rule's implementation.
    
    RELNOTES: None.
    PiperOrigin-RevId: 265728689

--- examples/xplatform/proto/main.swift
+++ examples/xplatform/proto/main.swift
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import Foundation
+import SwiftProtobuf
 import examples_xplatform_proto_example_proto
 
 let person = Person.with {
@@ -22,3 +23,18 @@ let person = Person.with {
 
 let data = try! person.serializedData()
 print(Array(data))
+
+let server = Server.with {
+  $0.name = "My Server"
+  $0.api.name = "My API"
+  let option = Google_Protobuf_Option.with {
+    $0.name = "Person Option"
+    if let value = try? Google_Protobuf_Any(message: person) {
+      $0.value = value
+    }
+  }
+  $0.api.options.append(option)
+}
+
+let data2 = try! server.serializedData()
+print(Array(data2))
