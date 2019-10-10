3bedd2254b3188e0b6c1bf705f62632c815bc184
thesayyn <thesayyn@gmail.com>
2019-10-10 22:50:15 +0300

    fixup!

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "rules_swift",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/socketio/socket.io-client-swift", .branch("master")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .branch("master"))
+        .package(url: "https://github.com/socketio/socket.io-client-swift", .branch("master"))
     ]
 )
