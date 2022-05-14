36b1240d0cdc0d109b3b38744264dba1134ffbb3
Andrew Z Allen <me@andrewzallen.com>
2022-05-14 02:06:26 -0600

    Upgrade gRPC to 1.7.3
    This upgrades past the 0.X series into the 1.X series which changes the
    import from `SwiftGRPC` to simply `GRPC` (it's cleaner).

--- examples/xplatform/grpc/client_main.swift
+++ examples/xplatform/grpc/client_main.swift
@@ -14,19 +14,45 @@
 
 import Foundation
 import SwiftProtobuf
+import GRPC
+import NIOCore
+import NIOPosix
 import examples_xplatform_grpc_echo_proto
 import examples_xplatform_grpc_echo_client_services_swift
 
+// Setup an `EventLoopGroup` for the connection to run on.
+//
+// See: https://github.com/apple/swift-nio#eventloops-and-eventloopgroups
+let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+
+// Make sure the group is shutdown when we're done with it.
+defer {
+  try! group.syncShutdownGracefully()
+}
+
+// Configure the channel, we're not using TLS so the connection is `insecure`.
+let channel = try GRPCChannelPool.with(
+  target: .host("localhost", port: 9000),
+  transportSecurity: .plaintext,
+  eventLoopGroup: group
+)
+
 // Initialize the client using the same address the server is started on.
-let client = RulesSwift_Examples_Grpc_EchoServiceServiceClient(address: "0.0.0.0:9000",
-                                                               secure: false)
+let client = RulesSwift_Examples_Grpc_EchoServiceClient(channel: channel)
 
 // Construct a request to the echo service.
-var request = RulesSwift_Examples_Grpc_EchoRequest()
-request.contents = "Hello, world!"
-let timestamp = Google_Protobuf_Timestamp(date: Date())
-request.extra = try! Google_Protobuf_Any(message: timestamp)
+var request = RulesSwift_Examples_Grpc_EchoRequest.with {
+  $0.contents = "Hello, world!"
+  let timestamp = Google_Protobuf_Timestamp(date: Date())
+  request.extra = try! Google_Protobuf_Any(message: timestamp)
+}
+
+let call = client.echo(request)
 
 // Make the remote method call and print the response we receive.
-let response = try client.echo(request)
-print(response.contents)
+do {
+  let response = try call.response.wait()
+  print(response.contents)
+} catch {
+  print("Echo failed: \(error)")
+}
