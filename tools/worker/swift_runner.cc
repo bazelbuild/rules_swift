b6d5790ac4c0e7cd4001dea80d688f05944f3319
Tony Allevato <allevato@google.com>
2022-08-23 09:46:07 -0700

    Remove `-whole-module-optimization` and `-num-threads` flags when performing the layering check `swiftc` invocation.
    In some cases, the presence of these flags would generate an error `<unknown>:0: error: cannot specify -o when generating multiple output files`.
    
    PiperOrigin-RevId: 469483002

--- tools/worker/swift_runner.cc
+++ tools/worker/swift_runner.cc
@@ -135,14 +135,15 @@ int SpawnJob(const std::vector<std::string> &tool_args,
 // a flag).
 bool SkipLayeringCheckIncompatibleArgs(std::vector<std::string>::iterator &it) {
   if (*it == "-emit-module" || *it == "-emit-module-interface" ||
-      *it == "-emit-object" || *it == "-emit-objc-header") {
+      *it == "-emit-object" || *it == "-emit-objc-header" ||
+      *it == "-whole-module-optimization") {
     // Skip just this argument.
     return true;
   }
   if (*it == "-o" || *it == "-output-file-map" || *it == "-emit-module-path" ||
       *it == "-emit-module-interface-path" || *it == "-emit-objc-header-path" ||
-      *it == "-emit-clang-header-path") {
-    // This flag has a path after it that we also need to skip.
+      *it == "-emit-clang-header-path" || *it == "-num-threads") {
+    // This flag has a value after it that we also need to skip.
     ++it;
     return true;
   }
