569819bcc21cce8e1ed94194a11e2c1890bab426
Brentley Jones <github@brentleyjones.com>
2022-12-01 11:13:36 -0600

    Fix incremental compilation when generated header is nested in a directory (#945)

--- examples/apple/objc_interop/main.m
+++ examples/apple/objc_interop/main.m
@@ -17,7 +17,7 @@
 // rules automatically add "bazel-genfiles" to the include path, the file should
 // be imported using a workspace-relative path to access the Obj-C interface for
 // the Swift code.
-#import "examples/apple/objc_interop/Printer-Swift.h"
+#import "examples/apple/objc_interop/generated_header/Printer-Swift.h"
 
 int main(int argc, char **argv) {
   @autoreleasepool {
