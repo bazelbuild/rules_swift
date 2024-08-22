0e2287a3fef786f3bf3b5b7e01e08881678f5250
Ivan Golub <ivan.g.golub@gmail.com>
2024-08-22 15:54:27 -0700

    Swift invalidating when change private headers repro

---
+++ examples/apple/objc_interop/Private.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface Private : NSObject
+
+- (void)foo;
+
+@end
---
+++ examples/apple/objc_interop/Private.m
@@ -0,0 +1,10 @@
+#import "examples/apple/objc_interop/Private.h"
+
+@implementation Private 
+
+- (void)foo 
+{
+    NSLog(@"Private.foo");
+}
+
+@end
