f55dc7ce93486dca28e798513797775d476d6050
Corentin Kerisit <corentin.kerisit@gmail.com>
2025-01-15 16:51:52 +0100

    fix example

--- examples/runfiles/main.swift
+++ examples/runfiles/main.swift
@@ -1,17 +1,17 @@
 import BazelRunfiles
 
-let runfiles = try Runfiles.create()
+do {
+    let runfiles = try Runfiles.create()
+    // Runfiles lookup paths have the form `my_workspace/package/file`.
+    // Runfiles path lookup may throw.
+    let fileURL = try runfiles.rlocation("build_bazel_rules_swift/examples/runfiles/data/sample.txt")
+    print("file: \(fileURL)")
 
-// Runfiles lookup paths have the form `my_workspace/package/file`.
-// Runfiles path lookup may return nil.
-guard let runFile = runfiles.rlocation("build_bazel_rules_swift/examples/runfiles/data/sample.txt") else {
-    fatalError("couldn't resolve runfile")
+    // Runfiles path lookup may return a non-existent path.
+    let content = try String(contentsOf: fileURL, encoding: .utf8)
+
+    assert(content == "Hello runfiles")
+    print(content)
+} catch {
+     print("runfiles error: \(error)")
 }
-
-print(runFile)
-
-// Runfiles path lookup may return a non-existent path.
-let content = try String(contentsOf: runFile, encoding: .utf8)
-
-assert(content == "Hello runfiles")
-print(content)
