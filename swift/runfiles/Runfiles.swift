81e38961f1e414bf1246b6b881b1abe8513c8a5e
Kirill Yakimovich <kirill.yakimovich@gmail.com>
2025-11-18 14:07:10 -0800

    Make the Runfiles compatible with Swift 6 language mode

--- swift/runfiles/Runfiles.swift
+++ swift/runfiles/Runfiles.swift
@@ -215,8 +215,13 @@ public final class Runfiles {
 }
 
 // https://github.com/bazel-contrib/rules_go/blob/6505cf2e4f0a768497b123a74363f47b711e1d02/go/runfiles/global.go#L53-L54
+#if swift(>=5.10)
+nonisolated(unsafe) private let legacyExternalGeneratedFile = /bazel-out\/[^\/]+\/bin\/external\/([^\/]+)/
+nonisolated(unsafe) private let legacyExternalFile = /external\/([^\/]+)/
+#else
 private let legacyExternalGeneratedFile = /bazel-out\/[^\/]+\/bin\/external\/([^\/]+)/
 private let legacyExternalFile = /external\/([^\/]+)/
+#endif
 
 // Extracts the canonical name of the repository containing the file
 // located at `path`.
