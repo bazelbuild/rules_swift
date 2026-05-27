bf54293df496387acb475aa53a94b279ea434e59
Keith Smiley <keithbsmiley@gmail.com>
2026-05-27 16:49:50 -0700

    Add a mechanism to pass through arbitrary flags to other tools that the Swift worker invokes, and apply it to the generated header rewriter. (#1774)
    Cherry picks bb62c89a7bb2a9a90206f813821f12b47aabb3d8

--- tools/worker/swift_runner.h
+++ tools/worker/swift_runner.h
@@ -22,6 +22,7 @@
 #include <string>
 #include <vector>
 
+#include "absl/container/flat_hash_map.h"
 #include "tools/common/bazel_substitutions.h"
 #include "tools/common/temp_file.h"
 
@@ -163,6 +164,12 @@ class SwiftRunner {
   // The path to the generated header rewriter tool, if one is being used for
   // this compilation.
   std::string generated_header_rewriter_path_;
+
+  // A map containing arguments that should be passed through to additional
+  // tools that support them. Each key in the map represents the name of a
+  // recognized tool.
+  absl::flat_hash_map<std::string, std::vector<std::string>>
+      passthrough_tool_args_;
 
   // The Bazel target label that spawned the worker request, which can be used
   // in custom diagnostic messages printed by the worker.
